3204ce02143aa8fb8d0b60fe9c81f1069fa07dfe
Felipe Bergamin <felipebergamin6@gmail.com>
2021-03-14 19:01:14 -0300

    feat: implement new calc helpers

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,33 @@
-const sum = (...numbers) => numbers.reduce((prev, curr) => prev + curr, 0);
+const sum = (...numbers) => {
+  return numbers.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+};
+
+const minus = (...numbers) => {
+  return numbers.reduce((prev, curr) => {
+    return prev - curr;
+  }, 0);
+};
+
+const times = (...numbers) => {
+  return numbers.reduce((prev, curr) => {
+    return prev * curr;
+  }, 0);
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const pow = (a, b) => {
+  return Math.pow(a, b);
+};
 
 module.exports = {
   sum,
+  minus,
+  times,
+  divide,
+  pow,
 };
